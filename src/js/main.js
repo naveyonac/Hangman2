069891400d030317de645a98e69b7c93105d77b2
Evan Canoy <evan@Evans-MBP.fios-router.home>
2019-04-13 10:46:55 -0400

    functioning game, working on issue with double letters

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,4 @@
-// const key = document.querySelectorAll('.key')
-// const button = document.querySelector('.startGame')
-// var word
-// var wordArray
 
-// button.addEventListener('click', startGameFunction(evt))
-//     // evt.preventDefault()
-//     // let wordInput = prompt('Pick a word for player 2 to guess!')
-//     // console.log(wordInput)
-//     // evt.stopImmediatePropagation() //stops the prompt form popping up a million times.
-//     // word = wordInput
-// // }) //this will get the word from player one to use.
-// // for (i=0;i < 26; i++) {
-// //     key[i].addEventListener('click', function(evt){
-// //         evt.stopImmediatePropagation()
-// //         alert('Key Was Clicked!')
-// //     })} 
-// //adds event listeners to all of the keyboard keys for input selection
-
-// function startGameFunction() {
-//     evt.preventDefault()
-//     let wordInput = prompt('Pick a word for player 2 to guess!')
-//     console.log(wordInput)
-// }
-
-// while (i=0; i < 26; i++) {
-//     key[i].addEventListener('click', function(){
-//         alert('Key Was Clicked!')
-//     }
-// }
 let keyBoard = document.querySelector('.keyboard')
 let keyboardKeys = document.querySelectorAll('.key')
 let startButton = document.querySelector('.startGame')
@@ -98,25 +69,25 @@ function addKeyboardListeners(wordSplit) {
 
 function changeImages(wrongGuesses) {
     if (wrongGuesses === 0) {
-        document.querySelector('.manImage').src = 'http://localhost:59859/img1.97b391c3.png'
+        document.querySelector('.manImage').src = 'src/img/img1.png'
     }
     else if (wrongGuesses === 1) {
-        document.querySelector('.manImage').src = 'http://localhost:59859/img2.c13fb1c1.png'
+        document.querySelector('.manImage').src = 'src/img/img2.png'
     }
     else if (wrongGuesses === 2) {
-        document.querySelector('.manImage').src = 'http://localhost:59859/img3.c54a28a8.png'
+        document.querySelector('.manImage').src = 'src/img/img3.png'
     }
     else if (wrongGuesses === 3) {
-        document.querySelector('.manImage').src = 'http://localhost:59859/img4.dd0d453f.png'
+        document.querySelector('.manImage').src = 'src/img/img4.png'
     }
     else if (wrongGuesses === 4) {
-        document.querySelector('.manImage').src = 'http://localhost:59859/img5.9002a70c.png'
+        document.querySelector('.manImage').src = 'src/img/img5.png'
     }
     else if (wrongGuesses === 5) {
-        document.querySelector('.manImage').src = 'http://localhost:59859/img6.dfab2afd.png'
+        document.querySelector('.manImage').src = 'src/img/img6.png'
     }
     else {
-        document.querySelector('.manImage').src = 'http://localhost:59859/img7.97683ac8.png'
+        document.querySelector('.manImage').src = 'src/img/img7.png'
         document.querySelector('.message').innerHTML = "Game Over, You Lose!" //once the last image is shown, this message will pop up.
     }
 }
